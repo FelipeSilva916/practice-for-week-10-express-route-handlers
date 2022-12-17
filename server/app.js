3f055dc19a103f4ba521d6a0e2f7b2445d7ac414
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-12-17 14:25:11 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,8 +23,28 @@ const {
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // Your code here
+
+app.use((req, res, next) => {
+  console.log("Body:", req.body);
+  next();
+});
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/artists", async (req, res) => {
+  const artists = await getAllArtists();
+  res.send(artists);
+});
+
+app.post("/artists", async (req, res) => {
+  const artist = await addArtist(req.body);
+  res.send(artist);
+});
 
 const port = 5000;
 app.listen(port, () => console.log("Server is listening on port", port));
